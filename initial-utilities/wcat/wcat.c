6cab6aa039330d1559752b2b7b800822798e32fa
Nguyen Thanh <thanhnv812@Nguyens-MacBook-Air.local>
2020-03-22 23:16:00 +0700

    Update P1a wcat

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]){
-    
-    FILE* fp = fopen(argv[1], "r");
-    if(fp == NULL){
-        printf("Cannot open the file\n");
-        exit(1);
-    }else{
-        char c;
-        while(1){
-            c = fgetc(fp);
-            if(c != EOF){
-                printf("%c", c);
-            }else{
+// The readFile func read file from provided path char by char
+// and print to stdout it's content
+// finally close the file
+
+#define BUFFER_SIZE 256
+char buffer[BUFFER_SIZE];
+
+void readFile(char *path)
+{
+    FILE *fp = fopen(path, "r");
+    if (fp == NULL)
+    {
+        printf("my-cat: cannot open file\n");
+        return;
+    }
+    else
+    {
+        while (1)
+        {
+            char* c = fgets(buffer, BUFFER_SIZE, fp);
+            if (c != NULL)
+            {
+                printf("%s", c);
+            }
+            else
+            {
                 break;
             }
         }
     }
 
     fclose(fp);
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+
+    for (int i = 1; i < argc; i++)
+    {
+        readFile(argv[i]);
+    }
+
     return 0;
 }
